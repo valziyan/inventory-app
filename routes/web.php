947e4ec42c4f84fbaa66bb332180d7bf3cf59e88
Ryan <valziyan@gmail.com>
2024-02-07 19:11:34 +0800

    created /profile2 route

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/profile2', function () {
+    return 'Welcome to my Profile';
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
